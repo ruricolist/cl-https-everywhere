0499bd261931c0c7eefe9746700121e5d94c29ac
Paul M. Rodriguez <pmr@ruricolist.com>
2016-11-10 18:14:27 -0600

    Require ternary if.

--- package.lisp
+++ package.lisp
@@ -3,6 +3,11 @@
 (defpackage #:cl-https-everywhere
   (:use #:cl #:alexandria #:serapeum)
   (:nicknames #:https-everywhere)
+  (:shadow :if)
   (:export
    #:rewrite-uri))
 
+(in-package :cl-https-everywhere)
+
+(defmacro if (test then else)
+  `(cl:if ,test ,then ,else))
