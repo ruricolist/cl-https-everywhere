1856b1b4012ef4cf24717005610ab831792251b9
Paul M. Rodriguez <pmr@ruricolist.com>
2017-01-30 18:00:27 -0600

    Minor: dead code.

--- build.lisp
+++ build.lisp
@@ -19,7 +19,6 @@
 (overlord:file-target rulesets "rulesets.xml" (temp)
   (with-output-to-file (out temp :if-exists :rename-and-delete
                                  :external-format :utf-8)
-    #+ () (:print "Concatenating rulesets.xml.")
     (format out "<rulesets>~%")
     (dolist (file +ruleset-files+)
       (with-input-from-file (in file :external-format :utf-8)
