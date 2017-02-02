20efecc81b33414b67ea2b2cd53970d3c0194be2
Paul M. Rodriguez <pmr@ruricolist.com>
2017-02-02 16:37:43 -0600

    Move update into the init form.

--- build.lisp
+++ build.lisp
@@ -1,32 +1,58 @@
 (in-package #:cl-https-everywhere)
 
-(overlord:define-constant +rules-dir+
-    (merge-pathnames "https-everywhere/rules/")
-  :test #'equal)
+(defmacro with-feedback ((start end) &body body)
+  `(progn
+     (format t "~&~@?" ,start)
+     (force-output)
+     (multiple-value-prog1 (progn ,@body)
+       (format t "~@?~%" ,end)
+       (force-output))))
 
-(overlord:defconst/deps +ruleset-files+
+(overlord:define-constant +https-everywhere-repo+
+  "https://github.com/EFForg/https-everywhere.git")
+
+(overlord:directory-target https-everywhere "https-everywhere/"
+  (progn
+    (when (uiop:directory-exists-p https-everywhere)
+      (uiop:delete-directory-tree https-everywhere :validate t))
+    (with-feedback ("Fetching rules..." "Fetched rules")
+      (:run `("git" "clone" "--depth=1" ,+https-everywhere-repo+))))
+  (:depends-on '+https-everywhere-repo+))
+
+(overlord:defvar/deps *ruleset-files*
     ;; uiop:directory-files is too slow
-    (directory
-     (make-pathname :defaults +rules-dir+
-                    :type "xml"
-                    :name :wild))
-  (:depends-on '+rules-dir+)
-  (unless (uiop:directory-exists-p +rules-dir+)
-    (uiop:run-program "git submodule update --init --depth=1")
-    (assert (uiop:directory-exists-p +rules-dir+)))
-  (:depends-on +rules-dir+))
+    (progn
+      (overlord/http:online-only ()
+        (with-feedback ("Updating rules..." "Rules updated.")
+          (:run
+           "cd https-everywhere; git fetch --depth 1; git reset --hard origin/master")))
+      (directory
+       (path-join https-everywhere
+                  #p"rules/"
+                  (make-pathname :type "xml" :name :wild))))
+  (:depends-on '+https-everywhere-repo+)
+  (:depends-on https-everywhere))
 
 (overlord:file-target rulesets "rulesets.xml" (temp)
   (with-output-to-file (out temp :if-exists :rename-and-delete
                                  :external-format :utf-8)
-    (format t "~&Concatenating rulesets.xml.~%")
-    (format out "<rulesets>~%")
-    (dolist (file +ruleset-files+)
-      (with-input-from-file (in file :external-format :utf-8)
-        (copy-stream in out)))
-    (format out "~%</rulesets>"))
-  (:depends-on '+ruleset-files+)
-  (:depends-on +ruleset-files+))
+    (with-feedback ("Concatenating rulesets.xml..." "Built rulesets.xml")
+      (format out "<rulesets>~%")
+      (dolist (file *ruleset-files*)
+        (with-input-from-file (in file :external-format :utf-8)
+          (copy-stream in out)))
+      (format out "~%</rulesets>")))
+  (:depends-on '*ruleset-files*)
+  (:depends-on *ruleset-files*))
+
+(overlord:deftask clean ()
+  (uiop:delete-directory-tree
+   #p"https-everywhere/"
+   :validate (op (uiop:subpathp _ (asdf:system-relative-pathname :cl-https-everywhere "")))))
+
+(overlord:deftask maintainer-clean ()
+  (uiop:delete-file-if-exists rulesets)
+  (:depends-on 'clean))
 
 (defparameter *rulesets*
   (overlord:require-as :cl-https-everywhere/rulesets-file "rulesets"))
